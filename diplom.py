9f66d896df9519e6a281476f3435d5d275cc7ccd
Kotoffey <moiseevkasimov@gmail.com>
2025-02-16 00:02:32 +0300

    revision 'diplom.py'

--- diplom.py
+++ diplom.py
@@ -1,6 +1,9 @@
+from dbm import error
+import sys
 import requests
 import configparser
 from tqdm import tqdm
+from pprint import pprint
 import json
 
 congig = configparser.ConfigParser()
@@ -10,6 +13,8 @@ vktoken = congig['Tokens']['vktoken']
 yatoken  = congig['Tokens']['yatoken']
 folder = input('введите название папки на яндекс диске: ')
 qty = int(input('введите количество скачиваемых фото: '))
+
+
 class VK :
     url = 'https://api.vk.com/method/'
 
@@ -28,39 +33,17 @@ class VK :
             'photo_sizes': '1',
             'extended' : 1
         }
+        try:
+            response = requests.get(f'{self.url}/photos.get',params = self.params).json()
+            #print(response)
+            photos = response['response']['items']
+           #  print(response)
 
-        response = requests.get(f'{self.url}/photos.get',params = self.params).json()
-        #print(response)
-        photos = response['response']['items']
-
-        likes_list =[]
-        final_list = []
-        for i in tqdm(range(qty)):
-
-                #pprint(photo)
-                #print (f'фото номер: {i}')
-                max_photo = max(photos[i]['sizes'],key = lambda x:x['height'])     # максимальный размер по ширине
-                urlphoto = (max_photo['url'])
-                likes = photos[i]['likes']['count']
-                if likes in likes_list:   # проверка на одинаковое количество лайков
-                    likes = f'{likes}({i})'
-                #print(urlphoto)
-                #print(f'это фото набрало {likes} лайков ')
-                ya.savephoto_url(urlphoto, f'{folder}/{likes}.jpg')
-                likes_list.append(likes)
-
-                finaldict = {"file_name":f'{likes}.jpg',
-                             'size':max_photo['type']
-
-                }
-
-                final_list.append(finaldict)
-        print(f'загрузка {qty} файлов в папку {folder} завершена успешно')
-        #print(final_list)
-        final = json.dumps(final_list)
-        with open('result.json','w') as f:
-            f.write(final)
-
+            return photos
+        except KeyError:
+            print('Неверный VKID или TOKEN , проверьте пожалуйста корректность ввода')
+        except TypeError:
+            print('видимо количество фотографий в альбоме меньше, чем вы хотите скачать')
 
 
 class YaDisk:
@@ -74,7 +57,7 @@ class YaDisk:
         params = {'path': folder}
 
         response = requests.put(url, headers=headers, params=params)
-        print(response.status_code)
+        #print(response.status_code)
 
     def savephoto_url(self,urlphoto,folder):
         self.folder = folder
@@ -87,11 +70,47 @@ class YaDisk:
         response = requests.post(url, headers=headers, params=params)
 
 
+def processing_photo():
+    likes_list =[]
+    final_list = []
+    photo = vk.getphoto(qty)
+    try:
+        for i in tqdm(range(qty)):
 
+            max_photo = max(photo[i]['sizes'],key = lambda x:x['height'])     # максимальный размер по ширине
+            #print(max_photo)
+            urlphoto = (max_photo['url'])
+            likes = photo[i]['likes']['count']
+            if likes in likes_list:   # проверка на одинаковое количество лайков
+                 likes = f'{likes}({i})'
+            ya.savephoto_url(urlphoto, f'{folder}/{likes}.jpg')
+            likes_list.append(likes)
+
+            finaldict = {"file_name":f'{likes}.jpg',
+                         'size':max_photo['type']
+        }
+            final_list.append(finaldict)
+        print(f'загрузка {qty} файлов в папку {folder} завершена успешно')
+
+    except IndexError:
+        pass
+    except TypeError:
+        pass
+
+#print(final_list)
+    final = json.dumps(final_list)
+    with open('result.json','w') as f:
+        f.write(final)
 ya = YaDisk(yatoken)
-ya.add_folder(folder)
 vk = VK(vkid, vktoken)
-vk.getphoto(qty)
+ya.add_folder(folder)
+processing_photo()
+
+
+
+
+
+
 
 
 
